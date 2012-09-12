49750afd7bc59aa1cf00d695609876be3e4df4df
Mike Schober <mschober@cj.com>
2012-09-11 21:09:37 -0700

    initial commit

---
+++ src/main/groovy/com/busgo/Route.groovy
@@ -0,0 +1,7 @@
+package com.busgo
+
+class Route {
+    def show() {
+        println 'Hello World'
+    }
+}
---
+++ src/test/groovy/com/busgo/RouteTest.groovy
@@ -0,0 +1,10 @@
+package com.busgo
+
+import groovy.util.GroovyTestCase
+
+class RouteTest extends GroovyTestCase {
+
+    void testShow() {
+        new Example().show()
+    }
+}
