938fc8bfdacd6a7b92e3027afdba7718fe14c984
Mike Schober <mschober@cj.com>
2012-09-21 20:50:11 -0700

    quick spike to add groovy sql and run the main class

--- src/main/groovy/com/busbeep/Catcher.groovy
+++ src/main/groovy/com/busbeep/Catcher.groovy
@@ -1,9 +1,20 @@
 package com.busbeep
 
+import groovy.sql.Sql
+
 class Catcher {
 
     private Route route
     private int stop
+
+    public static void main(String[] args){
+        def sql = Sql.newInstance("jdbc:mysql://localhost:3306/busbeep_dev", "busbeep",
+                              "DannyBoy6812", "com.mysql.jdbc.Driver")
+
+        sql.eachRow("select * from todo") {
+            println "Gromit likes ${it.title}"
+        }
+    }
 
     def subscribe(route, stop) {
         this.route = route
